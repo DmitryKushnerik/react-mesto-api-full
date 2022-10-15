c2226c80f99840873e34044a3c61def9acfff5c0
Dmitry Kushnerik <kushnerik.dmitry@yandex.ru>
2022-10-15 18:42:09 +0700

    fix: проверить карточки 3

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -6,7 +6,7 @@ const Card = require('../models/card');
 // Получить все карточки
 module.exports.getAllCards = (req, res, next) => {
   Card.find({})
-    .then((cards) => res.send({ cards }))
+    .then((cards) => res.send({ cards.cards }))
     .catch(next);
 };
 
